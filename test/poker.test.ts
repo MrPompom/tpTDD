079f8874f1c062988d534ce418e94ecaf34eb040
Bastien PROMPSY <bastienprompsy@gmail.com>
2025-03-04 14:37:08 +0100

    init jest + test trouver combinaison la plus haute

---
+++ test/poker.test.ts
@@ -0,0 +1,8 @@
+import { evaluerMain } from "../src/poker";
+
+describe("Évaluation des mains de poker", () => {
+  it("reconnaît une carte haute", () => {
+    const main = ["2C", "5K", "9T", "JP", "KC"];
+    expect(evaluerMain(main)).toBe("Carte Haute");
+  });
+});
