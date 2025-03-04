e5a061027457838110e6f77006426ded49c294c4
Bastien PROMPSY <bastienprompsy@gmail.com>
2025-03-04 14:45:15 +0100

    Ajout des test pour chaque combinaison du poker

--- test/poker.test.ts
+++ test/poker.test.ts
@@ -12,3 +12,41 @@ it("reconnaît une paire", () => {
   expect(evaluerMain(main)).toBe("Paire");
 });
 
+it("reconnaît une double paire", () => {
+  const main = ["2C", "2K", "9T", "9P", "KC"];
+  expect(evaluerMain(main)).toBe("Double Paire");
+});
+it("reconnaît un brelan", () => {
+  const main = ["2C", "2K", "2T", "9P", "KC"];
+  expect(evaluerMain(main)).toBe("Brelan");
+});
+
+it("reconnaît une suite", () => {
+  const main = ["5C", "6K", "7T", "8P", "9C"];
+  expect(evaluerMain(main)).toBe("Suite");
+});
+
+it("reconnaît une couleur", () => {
+  const main = ["2C", "5C", "9C", "JC", "KC"];
+  expect(evaluerMain(main)).toBe("Couleur");
+});
+
+it("reconnaît un full", () => {
+  const main = ["3C", "3K", "3T", "9P", "9C"];
+  expect(evaluerMain(main)).toBe("Full");
+});
+
+it("reconnaît un carré", () => {
+  const main = ["4C", "4K", "4T", "4P", "9C"];
+  expect(evaluerMain(main)).toBe("Carré");
+});
+
+it("reconnaît une quinte flush", () => {
+  const main = ["5C", "6C", "7C", "8C", "9C"];
+  expect(evaluerMain(main)).toBe("Quinte Flush");
+});
+
+it("reconnaît une quinte flush royale", () => {
+  const main = ["10C", "JC", "QC", "KC", "AC"];
+  expect(evaluerMain(main)).toBe("Quinte Flush Royale");
+});
